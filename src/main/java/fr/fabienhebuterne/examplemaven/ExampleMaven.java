92bb350afe031b136bda60ee74a2463db44262a7
Fabien <contact@fabien-hebuterne.fr>
2020-11-11 15:30:01 +0100

    init

---
+++ src/main/java/fr/fabienhebuterne/examplemaven/ExampleMaven.java
@@ -0,0 +1,16 @@
+package fr.fabienhebuterne.examplemaven;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ExampleMaven extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
